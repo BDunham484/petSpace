dfe6a4265eb0ca67465636de40bd5819e14307a7
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-08-09 20:37:33 -0500

    add post.js

---
+++ models/post.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+module.exports = function(sequelize, DataTypes) {
+    var Post = sequelize.define("Post", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: " ",
+        validate: {
+          len: [0-150]
+        }
+      },
+      imageURL: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      animal: {
+        type: DataTypes.STRING,
+        defaultValue: "Other"
+      },
+   
+    });
+  
+    Post.associate = function(models) {
+        Post.belongsTo(models.User, {foreignKey: { allowNull: false }})
+  
+        Post.hasMany(models.Comment) 
+  
+  
+    }
+  
+  
+    return Post;
+  };
