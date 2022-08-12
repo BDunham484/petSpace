adaf4f1a0dabf74e075a74014b719aa936d71f79
ThereWolf <bdunham484@gmail.com>
2022-08-12 15:23:44 -0500

    route tweaks

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,22 +12,24 @@ const { Post, User, Comment } = require('../models');
 //                 'id',
 //                 'user_id',
 //                 'post_image',
-//                 'content',
+//                 'post_text',
+//                 'pet_name',
+//                 'pet_type',
 //                 'created_at'
 //             ],
-//             include: [{
-//                     model: Comment,
-//                     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                     include: {
-//                         model: User,
-//                         attributes: ['username']
-//                     }
-//                 },
-//                 {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             ]
+//             // include: [{
+//             //         model: Comment,
+//             //         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//             //         include: {
+//             //             model: User,
+//             //             attributes: ['username']
+//             //         }
+//             //     },
+//             //     {
+//             //         model: User,
+//             //         attributes: ['username']
+//             //     }
+//             // ]
 //         })
 //         .then(dbPostData => {
 //             const posts = dbPostData.map(post => post.get({
