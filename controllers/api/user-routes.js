f4afd38f693de4a06285512bce8495146c03a407
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-08-12 15:52:18 -0500

    fixing issue

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -44,19 +44,11 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => {
-        req.session.save(() => {
-          req.session.user_id = dbUserData.id;
-          req.session.username = dbUserData.username;
-          req.session.loggedIn = true;
-      
-          res.json(dbUserData);
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
         });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
 
 // POST /api/users/login
@@ -77,19 +69,10 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
-        
-        req.session.save(() => {
-            // declare session variables
-            req.session.user_id = dbUserData.id;
-            req.session.username = dbUserData.username;
-            req.session.loggedIn = true;
-            
-            res.json({ user: dbUserData, message: 'You are now logged in!' });
-        });
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
 });
-
-
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
