7f92df3290ea1ffd38d7d023ff9f5ff099309026
ThereWolf <bdunham484@gmail.com>
2022-08-15 11:39:52 -0500

    add nodemon to deps

--- public/javascript/like.js
+++ public/javascript/like.js
@@ -1,12 +1,12 @@
-async function likeClickHandler(event) {
-    event.preventDefault();
+async function likeClickHandler() {
+    // event.preventDefault();
     console.log('like button clicked');
     // const id = window.location.toString().split('/')[
     //     window.location.toString().split('/').length - 1
     // ];
 
     const id = document.querySelector('textarea[id="hidden_id"]').value.trim();
-    
+    console.log(id);
     const response = await fetch('/api/posts/like', {
         method: 'PUT',
         body: JSON.stringify({
