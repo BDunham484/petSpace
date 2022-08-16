f8dde0e95b9d9c45cb7b9f1f2e926fcd06543ae9
ThereWolf <bdunham484@gmail.com>
2022-08-15 19:58:28 -0500

    add edit post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,7 +4,6 @@ const { Post, User, Comment } = require('../models');
 
 // get all posts for dashboard
 router.get('/', (req, res) => {
-    console.log(req.body)
     Post.findAll({
             where: {
                 user_id: req.session.user_id
@@ -36,10 +35,7 @@ router.get('/', (req, res) => {
             ]
         })
         .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({
-                plain: true
-            }));
-            console.log(posts)
+            const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('dashboard', {
                 posts,
                 loggedIn: true
@@ -48,55 +44,58 @@ router.get('/', (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
-            res.redirect('/signup');
         });
 });
 
-// router.get('/edit/:id', (req, res) => {
-//     Post.findOne({
-//             where: {
-//                 id: req.params.id
-//             },
-//             attributes: [
-//                 'id',
-//                 'title',
-//                 'content',
-//                 'created_at'
-//             ],
-//             include: [{
-//                     model: Comment,
-//                     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                     include: {
-//                         model: User,
-//                         attributes: ['username']
-//                     }
-//                 },
-//                 {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             ]
-//         })
-//         .then(dbPostData => {
-//             if (!dbPostData) {
-//                 res.status(404).json({
-//                     message: 'No post found with this id'
-//                 });
-//                 return;
-//             }
-//             const post = dbPostData.get({
-//                 plain: true
-//             });
-//             res.render('edit-post', {
-//                 post,
-//                 loggedIn: true
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// })
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'user_id',
+                'post_image',
+                'post_text',
+                'pet_name',
+                'pet_type',
+                'created_at',
+                [sequelize.literal('(SELECT COUNT(*) FROM `like` WHERE post.id = `like`.post_id)'), 'liked_count']
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'No post found with this id'
+                });
+                return;
+            }
+            const post = dbPostData.get({
+                plain: true
+            });
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
 // router.get('/new', (req, res) => {
 //     res.render('add-post', {
