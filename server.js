986fa5d47d0676fff081d3d3d1c5b010b8443977
MKAtkinson <maxatkinson@ymail.com>
2022-08-17 16:03:31 -0500

    Fixing branch for up to date with develop

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.use(fileUpload())
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
