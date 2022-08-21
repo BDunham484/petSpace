61f1ef4ec751353930431feef4e66ec0f14dcf7f
ThereWolf <bdunham484@gmail.com>
2022-08-21 14:34:16 -0500

    logo tweaks

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: "secret",
-  cookie: {},
+  cookie: {
+    maxAge: ((1000*60)*60)
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
