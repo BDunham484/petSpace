4da08cd33d8a09e3eee5334154abb8636421ca68
ThereWolf <bdunham484@gmail.com>
2024-02-28 20:56:39 -0600

    update connection for render deploy

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,27 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-//if using heroku it makes a connection to jawsdb
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-    //if using locally it makes a connection to the database using credentials stored in an environment
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
+
+sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+        host: process.env.DB_HOST,
         dialect: 'mysql',
         port: 3306
-    });
-}
+    }
+);
+// //if using heroku it makes a connection to jawsdb
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+//     //if using locally it makes a connection to the database using credentials stored in an environment
+// } else {
+//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+// }
 
 module.exports = sequelize;
